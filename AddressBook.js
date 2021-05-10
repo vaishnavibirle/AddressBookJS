fdf2e8a3a70ec84ae1ed96d4e5002e7e155d6bfb
vaishnavi birle <vaishnavibirle1133@gmail.com>
2021-05-10 23:02:46 +0530

    [Vaishnavi] : CREATED Contact in address book

--- AddressBook.js
+++ AddressBook.js
@@ -1,1 +1,42 @@
-/**Welcome To Address Book Program */
+/**Welcome To Address Book Program 
+ * Creating a contact with first name , last name 
+ * address, city , state , zip , phone number and email
+*/
+class Contact {
+
+    firstName;
+    lastName;
+    address;
+    city;
+    state;
+    zip;
+    mobileNumber;
+    email;
+
+    constructor (firstName,lastName,address,city,state,zip,mobileNumber,email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+    }
+
+    toString() {
+        return "\nFirst Name    : " + this.firstName +
+        "\nLast Name     : " + this.lastName +
+        "\nAddress       : " + this.address +
+        "\nCity          : " + this.city +
+        "\nState         : " + this.state +
+        "\nZip           : " + this.zip +
+        "\nMobile Number : " + this.mobileNumber +
+        "\nEmail         : " + this.email;
+    }
+}
+console.log("<<<<<Welcome to AddressBook Application>>>>>");
+
+//UC1 - Ability to Create Contact in Address Book
+let vaishnavi = new Contact("Vaishnavi","Birle","IUDP","Washim","Maharashtra",444505,8668612060,"vaishnavi@gmail.com");
+console.log(vaishnavi.toString());
